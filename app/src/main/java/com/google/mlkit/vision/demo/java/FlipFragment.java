7d2868ae3461bb42c57958b37b81f00e71943bd2
Omari Felix <Allive1@live.com>
2020-11-23 22:43:53 -0500

    mlkit base

---
+++ app/src/main/java/com/google/mlkit/vision/demo/java/FlipFragment.java
@@ -0,0 +1,7 @@
+package com.google.mlkit.vision.demo.java;
+
+import androidx.fragment.app.Fragment;
+
+public class FlipFragment extends Fragment {
+
+}
--- smartVueTypeF/src/main/java/com/vsu/smartvuetypef/model/ParseFace.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.vsu.smartvuetypef.model;
-
-import com.parse.ParseClassName;
-import com.parse.ParseObject;
-
-@ParseClassName("ParseFace") 
-public class ParseFace extends ParseObject{
-
-}
